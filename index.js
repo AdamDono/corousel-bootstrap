cfce870d9acfe86a3227b69c922196f240adc6c8
AdamDono <111118088+AdamDono@users.noreply.github.com>
2024-04-14 04:37:38 +0200

    added questions on js

--- index.js
+++ index.js
@@ -11,4 +11,51 @@ const questions = [{
     ]
 },
 
-]
+//add the same question in the same format
+
+{
+    question: "whats the color of the ground?",
+    answers:[
+{Text: "belle", correct: false},
+{Text: "brown", correct: true},
+{Text: "blue", correct: false},
+{Text: "eve", correct: false},
+
+    ]
+
+}
+
+,
+
+//add the same question in the same format
+
+{
+    question: "whats the color of the skin?",
+    answers:[
+{Text: "belle", correct: false},
+{Text: "black", correct: true},
+{Text: "blue", correct: false},
+{Text: "eve", correct: false},
+
+    ]
+
+}
+
+,
+
+//add the same quest ion in the same format
+
+{
+    question: "whats the color of the eye?",
+    answers:[
+{Text: "belle", correct: false},
+{Text: "white", correct: true},
+{Text: "blue", correct: false},
+{Text: "eve", correct: false},
+
+    ]
+
+}
+
+
+]; 
