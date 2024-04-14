40227da64f1f0cda5f58d8c8eceb9ee3641ab53a
AdamDono <111118088+AdamDono@users.noreply.github.com>
2024-04-14 03:54:13 +0200

    added js file with the first question

---
+++ index.js
@@ -0,0 +1,14 @@
+//add a cost with a viriable
+
+const questions = [{
+    question: "whats the color of the sky?",
+    answers:[
+{Text: "belle", correct: false},
+{Text: "luci", correct: false},
+{Text: "blue", correct: true},
+{Text: "eve", correct: false},
+
+    ]
+},
+
+]
